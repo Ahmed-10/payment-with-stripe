1c014e950af8886d45de22a3d94e9c6e2dc231b1
Ahmed-10 <ahmedalaa.10@hotmail.com>
2021-03-01 18:34:33 +0200

    fix error

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
